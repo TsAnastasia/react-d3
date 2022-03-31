8f8c573d3c62dafb02ad1f1838a43d7c3fbc6559
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-03-31 16:08:28 +0300

    add tree member

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,124 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tree from "react-d3-tree";
 import { RawNodeDatum, TreeNodeDatum } from "react-d3-tree/lib/types/common";
 import styles from "./app.module.scss";
 import AddTreeMember from "./components/addTreeMember/AddTreeMember";
 import Modal from "./components/ui/modal/Modal";
 
+const translateData = {
+  x: window.innerWidth / 2 - 170,
+  y: window.innerHeight / 2 - 15,
+};
+
+const addChildrenTreeNode = (
+  tree: RawNodeDatum[],
+  node: TreeNodeDatum,
+  name: string
+): RawNodeDatum[] => {
+  const test = new Array(10)
+    .fill("b")
+    .map(() => {
+      const t = Math.random();
+      console.log(t);
+      return t;
+    })
+    .join("-");
+  console.log("node", test);
+  const queue: RawNodeDatum[] = [];
+
+  queue.unshift(tree[0]);
+
+  while (queue.length > 0) {
+    const curNode = queue.pop();
+
+    // console.log("curNode?.attributes?.id === node.__rd3t.id", curNode, node);
+
+    if (curNode?.attributes?.id === node.attributes?.id) {
+      curNode?.children?.push({
+        name,
+        attributes: {
+          id: Math.random(),
+        },
+        children: [],
+      });
+      return [...tree];
+    }
+
+    if (curNode?.children) {
+      const len = curNode?.children?.length;
+
+      for (let i = 0; i < len; i++) {
+        queue.unshift(curNode.children[i]);
+      }
+    }
+  }
+
+  return [...tree];
+};
+
 const App = () => {
-  const [tree, setTree] = useState<RawNodeDatum>({
-    name: "root",
-    children: [],
-  });
+  const [tree, setTree] = useState<RawNodeDatum[]>([
+    {
+      name: "root",
+      attributes: {
+        id: "3434",
+      },
+      children: [
+        {
+          name: "first",
+          attributes: {
+            id: "46163",
+          },
+          children: [],
+        },
+        {
+          name: "second",
+          attributes: {
+            id: "34",
+          },
+          children: [],
+        },
+        // {
+        //   name: "third",
+        //   attributes: {
+        //     id: "3434",
+        //   },
+        //   children: [],
+        // },
+      ],
+    },
+  ]);
 
   const [node, setNode] = useState<undefined | TreeNodeDatum>(undefined);
 
   const handleAddmemberClose = () => setNode(undefined);
 
+  const handleNodeClick = (node: { data: TreeNodeDatum }) => setNode(node.data);
+
   const handleAddTreeMember = (name: string) => {
     if (!node) return;
 
-    // setTree((s) =>
-    // s.map((n) => {
-    //   console.log(n);
-    //   if (n.name === node.name) {
-    //     const children: RawNodeDatum[] = [{ name, children: [] }];
-    //     return {
-    //       ...n,
-    //       children,
-    //     };
-    //   }
-    //   return n;
-    // })
-    // );
+    setTree((state) => {
+      // console.log("state", state);
+      return addChildrenTreeNode(state, node, name);
+    });
 
     setNode(undefined);
   };
+
+  useEffect(() => {
+    // console.log("tree", tree);
+  }, [node]);
 
   return (
     <main className={styles.main}>
       <Tree
         data={tree}
-        onNodeClick={(node) => setNode(node.data)}
-        translate={{ x: 30, y: 30 }}
+        onNodeClick={handleNodeClick}
+        translate={translateData}
+        orientation="horizontal"
+        pathFunc="elbow"
+        collapsible={false}
       />
       <Modal isOpen={!!node} onClose={handleAddmemberClose}>
         <AddTreeMember
