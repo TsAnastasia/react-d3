a3f34cf492fe7b43d119e5923e2d881bbf1f27bf
Anastasia Tsibernaya <anastasiacibernaja@yandex.ru>
2022-09-13 13:07:39 +0300

    fix scatter

--- src/pages/graphs/scatter/GraphsScatter.tsx
+++ src/pages/graphs/scatter/GraphsScatter.tsx
@@ -2,93 +2,114 @@ import * as d3 from "d3";
 import { FC, useEffect, useRef } from "react";
 import data from "./data.json";
 
+type NodeType = typeof data[number];
 const SCALE_SIZE = 30;
 
 const GraphsScatter: FC<{
   height?: number;
   width?: number;
   padding?: number;
-}> = ({ height = 300, width }) => {
+}> = ({ height = 300, width, padding = 20 }) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (containerRef.current) {
       const boxSize = containerRef.current.getBoundingClientRect();
-      const width = boxSize.width - SCALE_SIZE;
-      const height = boxSize.height - SCALE_SIZE;
+      const width = boxSize.width - SCALE_SIZE - 2 * padding;
+      const height = boxSize.height - SCALE_SIZE - 2 * padding;
 
+      // append the SVG object to the container
       const svg = d3
         .select(containerRef.current)
         .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", width + SCALE_SIZE + 2 * padding)
+        .attr("height", height + SCALE_SIZE + 2 * padding)
         .append("g")
-        .attr("transform", `translate(${SCALE_SIZE}, ${0})`);
+        .attr("transform", `translate(${SCALE_SIZE + padding},${padding})`);
 
+      // Add X axis
       const x = d3.scaleLinear().domain([4, 8]).range([0, width]);
       const xAxis = svg
         .append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x));
 
+      // Add Y axis
       const y = d3.scaleLinear().domain([0, 9]).range([height, 0]);
       const yAxis = svg.append("g").call(d3.axisLeft(y));
 
-      const clip = svg
+      // Add a clipPath: everything out of this area won't be drawn.
+      // const clip =
+      svg
         .append("defs")
         .append("SVG:clipPath")
         .attr("id", "clip")
         .append("SVG:rect")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", width + padding)
+        .attr("height", height + padding)
         .attr("x", 0)
-        .attr("y", 0);
+        .attr("y", -padding);
 
+      // Create the scatter variable: where both the circles and the brush take place
       const scatter = svg.append("g").attr("clip-path", "url(#clip)");
 
+      // Add circles
       scatter
         .selectAll()
         .data(data)
         .enter()
         .append("circle")
         .attr("cx", (d) => x(d.Sepal_Length))
-        .attr("cy", (d) => y(d.Petal_Length))
+        .attr("cy", function (d) {
+          return y(d.Petal_Length);
+        })
         .attr("r", 8)
         .style("fill", "#61a3a9")
         .style("opacity", 0.5);
 
-      const zoom = d3
-        .zoom()
-        .scaleExtent([0.5, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
-        .extent([
-          [0, 0],
-          [width, height],
-        ])
-        .on("zoom", (event) => {
-          // recover the new scale
-          const newX = event.transform.rescaleX(x);
-          const newY = event.transform.rescaleY(y);
+      // This add an invisible rect on top of the chart area. This rect can recover pointer events: necessary to understand when the user zoom
+      svg
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .call(
+          d3
+            .zoom()
+            .scaleExtent([0.5, 20])
+            .translateExtent([
+              [0, 0],
+              [width, height],
+            ])
+            .extent([
+              [0, 0],
+              [width, height],
+            ])
+            .on("zoom", (event) => {
+              // recover the new scale
+              const newX = event.transform.rescaleX(x);
+              const newY = event.transform.rescaleY(y);
 
-          // update axes with these new boundaries
-          xAxis.call(d3.axisBottom(newX));
-          yAxis.call(d3.axisLeft(newY));
+              // update axes with these new boundaries
+              xAxis.call(d3.axisBottom(newX));
+              yAxis.call(d3.axisLeft(newY));
 
-          // update circle position
-          scatter
-            .selectAll("circle")
-            .attr("cx", (d) => newX(d.Sepal_Length))
-            .attr("cy", function (d) {
-              return newY(d.Petal_Length);
-            });
-        });
+              // update circle position
+              scatter
+                .selectAll("circle")
+                .attr("cx", (d) => newX((d as NodeType).Sepal_Length))
+                .attr("cy", (d) => newY((d as NodeType).Petal_Length));
+            }) as any
+        );
     }
-  }, []);
+  }, [padding]);
 
   return (
     <div
       ref={containerRef}
       style={{ border: "1px solid black", width, height }}
-    ></div>
+    />
   );
 };
 
