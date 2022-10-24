cb73550139b89739dd1a1c25e766fdeee7d45e56
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-10-24 14:20:19 +0300

    delete planner2

--- src/pages/planner/PlannerPage.tsx
+++ src/pages/planner/PlannerPage.tsx
@@ -1,15 +1,13 @@
 import { TASKS } from "./libs/data";
 import Planner from "./planner/Planner";
 import Planner1 from "./planner1/Planner1";
-// import Planner2 from "./planner2/Planner2";
 import scss from "./plannerPage.module.scss";
 
 const PlannerPage = () => {
   return (
     <main className={scss.root}>
-      <Planner1 />
-      {/* <Planner2 /> */}
       <Planner data={TASKS} />
+      <Planner1 />
     </main>
   );
 };
